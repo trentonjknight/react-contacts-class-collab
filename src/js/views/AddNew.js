a1e004de42d71a924fb08d1b7e83e15b994f972b
Hernán García <hernanjkd@gmail.com>
2019-10-21 21:53:40 +0000

    progress

--- src/js/views/AddNew.js
+++ src/js/views/AddNew.js
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom";
 function AddNew() {
 	return (
 		<>
-			<div>
-				<h1>Add a New Contact</h1>
-				<input label="Full Name" type="text" placeholder="Enter Name" />
-				<input label="Email" type="text" placeholder="Enter Email" />
-				<input label="Phone" type="text" placeholder="Enter Phone #" />
-				<input label="Address" type="text" placeholder="Enter Address" />
-				<button className="btn btn-large btn-primary">Save Contact</button>
-				<Link to="/rigo">back to contacts</Link>
-			</div>
+			<form>
+				<div className="form-group row">
+					<h1>Add a New Contact</h1>
+					<input label="Full Name" type="text" placeholder="Enter Name" />
+					<input label="Email" type="text" placeholder="Enter Email" />
+					<input label="Phone" type="text" placeholder="Enter Phone #" />
+					<input label="Address" type="text" placeholder="Enter Address" />
+					<button className="btn btn-large btn-primary">Save Contact</button>
+					<Link to="/rigo">back to contacts</Link>
+				</div>
+			</form>
 		</>
 	);
 }
