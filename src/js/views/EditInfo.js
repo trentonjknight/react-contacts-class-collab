deb53ba03f6ebbcca85b0706d2acb486167cf737
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-10-23 18:31:30 +0000

    edit-change commit

--- src/js/views/EditInfo.js
+++ src/js/views/EditInfo.js
@@ -10,6 +10,26 @@ const EditInfo = props => {
 	const address = useRef(null);
 	const { store, actions } = useContext(Context);
 
+    const [ifullname, SetIfullname] = useState(store.contacts[props.match.params.id].full_name);
+	const [iemail, SetIemail] = useState(store.contacts[props.match.params.id].email);
+	const [iphone, SetIphone] = useState(store.contacts[props.match.params.id].phone );
+	const [iaddress, SetIaddress] = useState(store.contacts[props.match.params.id].address);
+
+    const [objContact, setObjContact] = useState();
+
+
+    setObjContact( {
+        fullname : ifullname,
+	    email : iemail ,
+	    phone : iphone ,
+	    address : iaddress
+    })
+
+
+    const SaveInfo = () => {
+          actions.saveInfo(objContact);
+    }
+
 	return (
 		<>
 			<div className="mx-auto">
@@ -19,43 +39,47 @@ const EditInfo = props => {
 						<label>Full Name</label>
 						<input
 							type="text"
-							value={store.contacts[props.match.params.id].full_name}
+							value={ifullname}
 							className="form-control"
 							placeholder="Enter Name"
 							ref={fullname}
+                            onChange={(event) => SetIfullname(event.target.value)}
 						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
 						<input
 							type="text"
-							value={store.contacts[props.match.params.id].email}
+							value={iemail}
 							className="form-control"
 							placeholder="Enter Email"
 							ref={email}
+                            onChange={(event) => SetIemail(event.target.value)}
 						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
 						<input
 							type="text"
-							value={store.contacts[props.match.params.id].phone}
+							value={iphone}
 							className="form-control"
 							placeholder="Enter Phone #"
 							ref={phone}
+                            onChange={(event) => SetIphone(event.target.value)}
 						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
 						<input
 							type="text"
-							value={store.contacts[props.match.params.id].address}
+							value={iaddress}
 							className="form-control"
 							placeholder="Enter Address"
 							ref={address}
+                            onChange={(event) => SetIaddress(event.target.value)}
 						/>
 					</div>
-					<button className="btn btn-large btn-primary" onClick={() => actions.saveInfo()}>
+					<button className="btn btn-large btn-primary" onClick={() => actions.SaveInfo()}>
 						Save Edited Information
 					</button>
 					<Link to="/rigo">back to contacts</Link>
